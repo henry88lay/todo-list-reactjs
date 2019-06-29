688fbb1897c05856730543cbb7c645122f2eb0db
Henry Lay <henry88lay@gmail.com>
2019-06-30 09:58:55 +1000

    adding the delete component for the todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -19,7 +19,7 @@ export class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
                     { title }
-                    <button style={btnStyle}>x</button>
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
         )
@@ -35,7 +35,7 @@ const btnStyle = {
     background: '#ff0000',
     color: '#fff',
     border: 'none',
-    padding: '5px 10px',
+    padding: '5px 9px',
     borderRadius: '50%',
     cursor: 'pointer',
     float: 'right'
