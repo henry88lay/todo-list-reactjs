7d22422f433f14c2c6083e437ea3847efa718c43
Henry Lay <henry88lay@gmail.com>
2019-06-29 22:42:55 +1000

    built out the Todoitem js file

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from "prop-types";
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div style={itemStyle}>
+                <p>{ this.props.todo.title }</p>
+            </div>
+        )
+    }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+    backgroundColor: '#f4f4f4'
+}
+
+export default TodoItem
