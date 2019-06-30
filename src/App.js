0ecca1fa896af730e77cc3c2e2808c7787d8ac74
Henry Lay <henry88lay@gmail.com>
2019-06-30 14:54:36 +1000

    coded in the backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/about';
 import uuid from 'uuid';
-
+import axios from 'axios';
+import './App.css';
 class App extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Clean workspace',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Work on Coding projects on react',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Built a business empire',
-        completed: false
-      }
-    ]
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState({ todos: res.data }))
   }
 
   // Toggle Complete
@@ -51,15 +42,21 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.todos)
     return (
-      <div className="App">
-        <div className="container">
-        <Header />
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+          <Header />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function about() {
     return (
-        <div>
-            
-        </div>
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList App v1.0.0. It is a part of a react crash course</p>
+        </React.Fragment>
     )
 }
